cc9374b168826f4e2fce1950a33f3eea5bcf225d
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-07 18:56:56 +0530

    dataTypes

---
+++ basics_01/dataType.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello") We are using node js not browser
+
+console.log("Raj")
+
+let name="Raja"
+let age=22
+let isLoggedIn=true
+
+
+// number =>2 to power 53
+// bigint
+// string=>""
+// boolean=> true//false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Raja")
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
