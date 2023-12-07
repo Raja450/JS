aee7dd78dea71f1d08de03b12fd629ec0ce3be82
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-07 17:34:46 +0530

    changes

---
+++ basics_01/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=14453
+let accountEmail="raja.sah552@gmail.com"
+var accountPassword="12345"
+accounCity="Kolkata"
+
+//accountId=2//
+accountEmail="mahakal45@gmail.com"
+accountPassword="54223"
+accounCity="Pune"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accounCity])
