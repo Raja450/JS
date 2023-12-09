e4359ddaf05bd6c0e0519c41ca606b3f1e0284a9
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-09 10:38:47 +0530

    changes

--- basics_01/conversionOperation.js
+++ basics_01/conversionOperation.js
@@ -4,3 +4,18 @@ console.log(typeof score);
 
 let valueInNumber =Number(score)
 console.log(typeof valueInNumber);
+
+// Operations//
+let value =3
+let negValue = -value
+console.log(negValue);
+
+let str1 ="hello"
+let str2 =" Raja"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + "2" +2);
+console.log(1 +2  + "2");
