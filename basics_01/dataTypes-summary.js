112fce976d9aa642311531c6d7e820e6d4aa664d
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-10 16:39:18 +0530

    changes

--- basics_01/dataTypes-summary.js
+++ basics_01/dataTypes-summary.js
@@ -29,3 +29,29 @@ const myFunction = function(){
 }
 
 console.log(typeof myFunction);
+
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myName = "Raja"
+
+let anotherName = myName
+anotherName = "Bikash"
+
+console.log(myName);
+
+console.log(anotherName);
+
+
+let userOne =
+{
+    email: "Raj@google.com",
+    upi:  "user@ybl"
+}
+
+userTwo = userOne
+
+userTwo.email = "Rajkr@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
