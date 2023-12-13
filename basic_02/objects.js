33c8a26b543d68883f23393b47c162c9bbe0ca43
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-13 17:27:23 +0530

    Object

---
+++ basic_02/objects.js
@@ -0,0 +1,15 @@
+// Singleton
+
+//object literals
+
+const jsUser = {
+    name: "Raja",
+    age: 22,
+    location: "Kolkata",
+    email: "raj822gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+
+}
+console.log(jsUser.email);
+console.log(jsUser["email"]);
