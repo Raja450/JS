ea4c68b239aa63d58abf14a1108e24db843dc2d1
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-14 08:02:27 +0530

    Object

--- basic_02/objects.js
+++ basic_02/objects.js
@@ -2,8 +2,14 @@
 
 //object literals
 
+
+const mySym =Symbol("key1")
+
+
 const jsUser = {
     name: "Raja",
+    "full name": "Raja Sah",
+    [mySym]: "myKey1",
     age: 22,
     location: "Kolkata",
     email: "raj822gmail.com",
@@ -13,3 +19,22 @@ const jsUser = {
 }
 console.log(jsUser.email);
 console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+console.log(jsUser [mySym]);
+
+jsUser.email ="dyt555@gmail.com"
+//Object.freeze(jsUser)
+jsUser.email = "pyg555@gmail.com"
+
+console.log(jsUser)
+
+
+jsUser.greeting = function() {
+    console.log("Hello js user");
+}
+
+jsUser.greetingTwo = function() {
+    console.log(`Hello Js USer, ${this.name}`);
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
