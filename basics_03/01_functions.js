2b9bb36c09a4d4694d9b2dd9c2e8dee9223d799f
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-16 08:22:40 +0530

    changes

--- basics_03/01_functions.js
+++ basics_03/01_functions.js
@@ -32,3 +32,33 @@ function addTwoNumbers(number1, number2) {
     }
     //console.log(loginUSerMessage("Raja"))
     console.log(loginUSerMessage("Raja"))
+
+
+
+    function calculateCartPrice(...num1){
+        return num1
+    }
+    console.log(calculateCartPrice(200, 400 ,500));
+
+    const user = {
+        username: "raja",
+        price: 199
+    }
+
+
+    function handleObject(anyobject){
+        console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+
+    }
+
+    handleObject(user)
+
+
+
+    const myNewArray = [200,300,500,600]
+
+    function returnSecondValue(getArray){
+        return getArray[0]
+    }
+    console.log(returnSecondValue(myNewArray));
