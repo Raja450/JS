d0c00d5f23ad292c5c4268f7d5edff3831fb3e1f
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-16 07:48:24 +0530

    changes

---
+++ basics_03/01_functions.js
@@ -0,0 +1,34 @@
+function sayMName() {
+    console.log("R");
+    console.log("A");
+    console.log("J");
+    console.log("A");
+}
+//sayMName()
+
+// function addTwoNumbers(number1, number2) {
+//      console.log(number1+number2);
+// }
+//addTwoNumbers(3, 4)
+
+function addTwoNumbers(number1, number2) {
+        
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+    
+    }
+    const result = addTwoNumbers(3, 5)
+    console.log("Result", result);
+
+
+    function loginUSerMessage(username ="sam"){
+        if(username === undefined){
+            console.log("Please enter username");
+            return
+
+        }
+        return `${username} just logged in`
+    }
+    //console.log(loginUSerMessage("Raja"))
+    console.log(loginUSerMessage("Raja"))
