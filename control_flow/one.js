22f4c79a8944f5890dad35aa540b24ae13fbfede
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-23 23:22:04 +0530

    changes

---
+++ control_flow/one.js
@@ -0,0 +1,18 @@
+// if
+
+const temperature =11
+
+if(temperature < 50){
+    console.log("less then 20");
+    
+}
+else{
+console.log("temperature is greater than 20");
+}
+
+const score = 200
+
+if(score > 100) {
+    const power = "fly"
+    console.log(`User poe: ${power}`);
+}
