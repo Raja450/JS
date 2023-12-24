7aa3949001c9d8140c5cd1b6da7ac6cf253a9c6c
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-24 23:20:44 +0530

    changes

--- control_flow/one.js
+++ control_flow/one.js
@@ -21,3 +21,19 @@ if(score > 100) {
 const balance = 1000
 
 if(balance >500) console.log("test");
+
+if(balance > 500) {
+    console.log("less than 500");
+} else if (balance <750) {
+    console.log("less than 750");
+} else if(balance <900) {
+    console.log("less than 900");
+}
+
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+}
