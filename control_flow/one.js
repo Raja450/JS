f2c517aca28a49cdb3663b78062ec52c9a33ce87
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-24 23:13:08 +0530

    changes

--- control_flow/one.js
+++ control_flow/one.js
@@ -14,5 +14,10 @@ const score = 200
 
 if(score > 100) {
     const power = "fly"
-    console.log(`User poe: ${power}`);
+    console.log(`User power: ${power}`);
 }
+
+
+const balance = 1000
+
+if(balance >500) console.log("test");
