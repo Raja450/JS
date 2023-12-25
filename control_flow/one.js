345467fb97f2d9f88f118d6fcf149a986ee30467
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-12-25 08:04:19 +0530

    changes

--- control_flow/one.js
+++ control_flow/one.js
@@ -33,7 +33,13 @@ if(balance > 500) {
 
 const userLoggedIn = true
 const debitCard = true
+const loggedInFromGoogle =false
+const loggedInFromEmail =true
 
-if(userLoggedIn && debitCard) {
+if(userLoggedIn && debitCard && 2==2) {
     console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
 }
